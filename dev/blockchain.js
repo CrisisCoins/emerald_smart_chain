e8c1522ac030fb3c45078364f8c6967a2e8c98af
CrisisCoins <68887433+CrisisCoins@users.noreply.github.com>
2023-03-09 18:52:58 -0800

    Created GenesisBlock

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,64 +1,77 @@
-const sha256 = require('sha256');
+const sha256 = require("sha256");
 
 function Blockchain() {
-    this.chain = [];
-    this.pendingTransactions = [];
+  this.chain = [];
+  this.pendingTransactions = [];
+
+  // Genesis block
+  this.createNewBlock(0, '0x00000000', '0x00000000');
 }
 
-Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
-   const newBlock = {
-        blockIndex: this.chain.length + 1,
-        timestamp: Date.now(),
-        transactions: this.pendingTransactions,
-        nonce: nonce,
-        hash: hash,
-        previousBlockHash: previousBlockHash,
-   };
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    blockIndex: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
 
-   this.pendingTransactions = [];
-   this.chain.push(newBlock);
+  this.pendingTransactions = [];
+  this.chain.push(newBlock);
 
-   return newBlock;
-}
+  return newBlock;
+};
 
+Blockchain.prototype.getPreviousBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
 
-Blockchain.prototype.getPreviousBlock = function() {
-    return this.chain[this.chain.length - 1];
-}
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
 
+  this.pendingTransactions.push(newTransaction);
 
-Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
-    const newTransaction = {
-        amount: amount,
-        sender: sender,
-        recipient: recipient,
-    };
+  return this.getPreviousBlock()["blockIndex"] + 1;
+};
 
-    this.pendingTransactions.push(newTransaction);
+Blockchain.prototype.hashBlock = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataAsString =
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+  const hash = sha256(dataAsString);
+  return hash;
+};
 
-    return this.getPreviousBlock()['blockIndex'] + 1
-}
+Blockchain.prototype.proofOfWork = function (
+  previousBlockHash,
+  currentBlockData
+) {
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
 
+  while (hash.substring(0, 4) !== "0000") {
+    nonce++;
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  }
 
-Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
-    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
-    const hash = sha256(dataAsString);
-    return hash;
-}
+  return nonce;
+};
 
-
-Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
-    let nonce = 0;
-    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-
-    while (hash.substring(0, 4)!== "0000") {
-        nonce++;
-        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-    }
-
-    return nonce;
-}
-
-
-
-module.exports = Blockchain; 
+module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,27 +1,4 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
-
-const previousBlockHash = '0x9898989898';
-const currentBlockData = [
-    {
-        amount: 20,
-        sender: '0x9898989898989',
-        recipient: '0x9898989898989'
-    },
-    {
-        amount: 120,
-        sender: '0x363633989898989',
-        recipient: '0x978585498989898989'
-    },
-    {
-        amount: 220,
-        sender: '0x925896398989898989',
-        recipient: '0x989894747489898989'
-    }
-];
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 286572));
-
-// Test Nonce Value: 286572
-// Generated hash from previous test result: 00003a2d2cc953b5c4e884ea8f075a5db10e0ca4117e113f773b1d00ca81236d
+console.log(bitcoin);
