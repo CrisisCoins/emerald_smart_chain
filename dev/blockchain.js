57442e13246069b05b036023ec12a9f364503422
CrisisCoins <68887433+CrisisCoins@users.noreply.github.com>
2023-03-09 16:56:32 -0800

    Created New Block Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -2,3 +2,19 @@ function Blockchain() {
     this.chain = [];
     this.newTransactions = [];
 }
+
+Blockchain.prototype.generateNewBlock = function(nonce, previousBlockHash, hash) {
+   const newBlock = {
+        blockIndex: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        nonce: nonce,
+        hash: hash,
+        previousBlockHash: previousBlockHash,
+   };
+
+   this.newTransactions = [];
+   this.chain.push(newBlock);
+
+   return newBlock;
+};
