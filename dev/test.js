aeaf0e1de3cd0d9826a566f76ae806189b27fd61
CrisisCoins <68887433+CrisisCoins@users.noreply.github.com>
2023-03-09 17:42:50 -0800

    Tested Create New Transaction Method

--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,15 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(5069634, '0x323435', '0x434565');
-bitcoin.createNewBlock(302068929, '0x989873', '0x124234');
-bitcoin.createNewBlock(506930206, '0x090808', '0x848373');
+bitcoin.createNewBlock(5069634, '0x90809080', '0x03040303', '0x43435464');
 
-console.log(bitcoin);
+bitcoin.createNewTransaction(10, '0x3333333', '0x99999999');
+
+bitcoin.createNewBlock(4369605, '0x9425259080', '0x033323040303', '06565x43435464');
+
+bitcoin.createNewTransaction(16, '0x9993333333', '0x99834999999');
+bitcoin.createNewTransaction(110, '0x8883333333', '0x93239999999');
+bitcoin.createNewTransaction(610, '0x3999333333', '0x9332349999999');
+
+bitcoin.createNewBlock(30206, '0x676767080', '0x1243323040303', '5x43435464');
+console.log(bitcoin.chain[2]);
