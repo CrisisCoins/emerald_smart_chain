aa1ffd75ed955dc56de89d3690a78adaf37a096a
CrisisCoins <68887433+CrisisCoins@users.noreply.github.com>
2023-03-11 11:08:12 -0800

    Installed Postman & Body-Parser dependencies

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,27 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function(req, res){
-  res.send('Hello Moto');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// get entire blockchain
+app.get('/blockchain', function(req, res) {
+    
 });
 
-app.listen(3000);
+// create a new transaction
+app.post('/transaction', function (req, res) {
+  console.log(req.body);
+    res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
+  })
+
+// mine a block
+app.get('/mine', function(req, res) {
+
+});
+  
+
+app.listen(3000, function() {
+    console.log('listening on port 3000!')
+});
